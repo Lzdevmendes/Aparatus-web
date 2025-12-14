b13f60f3d961cc0b7575594ccbbae3830c60b36a
Felipe Rocha <felipemota.rocha@gmail.com>
2025-12-14 15:28:27 -0300

    feat: enhance booking cancellation process with Stripe refund handling and update booking model

--- actions/cancel-booking.ts
+++ actions/cancel-booking.ts
@@ -6,9 +6,10 @@ import { returnValidationErrors } from "next-safe-action";
 import { prisma } from "@/lib/prisma";
 import { isFuture } from "date-fns";
 import { revalidatePath } from "next/cache";
+import Stripe from "stripe";
 
 const inputSchema = z.object({
-  bookingId: z.string().uuid(),
+  bookingId: z.uuid(),
 });
 
 export const cancelBooking = protectedActionClient
@@ -19,31 +20,49 @@ export const cancelBooking = protectedActionClient
         id: bookingId,
       },
     });
-
     if (!booking) {
       returnValidationErrors(inputSchema, {
         _errors: ["Agendamento não encontrado."],
       });
     }
-
     if (booking.userId !== user.id) {
       returnValidationErrors(inputSchema, {
         _errors: ["Você não tem permissão para cancelar este agendamento."],
       });
     }
-
     if (booking.cancelledAt) {
       returnValidationErrors(inputSchema, {
         _errors: ["Este agendamento já foi cancelado."],
       });
     }
-
     if (!isFuture(booking.date)) {
       returnValidationErrors(inputSchema, {
         _errors: ["Não é possível cancelar um agendamento passado."],
       });
     }
-
+    if (booking.stripeChargeId) {
+      if (!process.env.STRIPE_SECRET_KEY) {
+        returnValidationErrors(inputSchema, {
+          _errors: ["Chave de API do Stripe não encontrada."],
+        });
+      }
+      try {
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+          apiVersion: "2025-07-30.basil",
+        });
+        await stripe.refunds.create({
+          charge: booking.stripeChargeId,
+          reason: "requested_by_customer",
+        });
+      } catch (error) {
+        console.error("Erro ao processar o reembolso do agendamento", error);
+        returnValidationErrors(inputSchema, {
+          _errors: [
+            "Erro ao processar o reembolso do agendamento. Por favor, tente novamente.",
+          ],
+        });
+      }
+    }
     const cancelledBooking = await prisma.booking.update({
       where: {
         id: bookingId,
@@ -52,9 +71,7 @@ export const cancelBooking = protectedActionClient
         cancelledAt: new Date(),
       },
     });
-
     revalidatePath("/");
     revalidatePath("/bookings");
-
     return cancelledBooking;
   });
